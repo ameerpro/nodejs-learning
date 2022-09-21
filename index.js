edcaeddfd9cd32044c137a1a57306a0e1a7630f3
Ameer Hamza <hamza@edkasa.com>
2022-09-21 11:40:41 +0500

    Events Emitter

--- index.js
+++ index.js
@@ -1,56 +1,30 @@
 const express = require("express");
-const multer = require("multer");
-require("./config");
-const Product = require("./product");
-
 const app = express();
+const EventEmitter = require("events");
+const event = new EventEmitter();
+
+let count = 0;
+
 app.use(express.json());
 
-app.post("/create", async (req, resp) => {
-  let data = new Product(req.body);
-  let result = await data.save();
-  resp.send(result);
+event.on("CountAPI", () => {
+  count++;
+  console.log("event called " + count);
 });
 
-app.get("/products", async (req, resp) => {
-  let result = await Product.find();
-  resp.send(result);
+app.get("/", (req, resp) => {
+  event.emit("CountAPI");
+  resp.send("api called");
 });
 
-app.delete("/delete/:_id", async (req, resp) => {
-  let result = await Product.deleteOne(req.params);
-  resp.send(result);
+app.get("/search", (req, resp) => {
+  event.emit("CountAPI");
+  resp.send("search api called");
 });
 
-app.put("/update/:_id", async (req, resp) => {
-  let result = await Product.updateOne(req.params, { $set: req.body });
-  resp.send(result);
-});
-
-app.get("/search/:key", async (req, resp) => {
-  let result = await Product.find({
-    $or: [
-      { name: { $regex: req.params.key } },
-      { category: { $regex: req.params.key } },
-      { brand: { $regex: req.params.key } },
-    ],
-  });
-  resp.send(result);
-});
-
-const uploadFile = multer({
-  storage: multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "uploads");
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.fieldname + "-" + Date.now() + ".pdf");
-    },
-  }),
-}).single("file");
-
-app.post("/upload", uploadFile, (req, resp) => {
-  resp.send("File uploaded");
+app.get("/update", (req, resp) => {
+  event.emit("CountAPI");
+  resp.send("update api called");
 });
 
 app.listen(5500);
